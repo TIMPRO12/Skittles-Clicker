a2dbe9bc5ae63a2d9cd6569db2111ccb1de5edc3
TIMPRO12 <coolgoldenfreddy19@gmail.com>
2024-05-29 14:50:36 -0400

    fixed NaN

--- game.js
+++ game.js
@@ -1,26 +1,32 @@
 let points = 0
 let pointsTracker = document.getElementById("pointsTracker")
 let buybag = 0
-let buybagAmmount = document.getElementById("buybag")
+let buybagAmmount = document.getElementById("Bag")
 let buybigbag = 0
-let buybigbagAmmount = document.getElementById("buybigbag")
+let buybigbagAmmount = document.getElementById("BigBag")
 let buydualbag = 0
-let buydualbagAmmount = document.getElementById("buydualbig")
+let buydualbagAmmount = document.getElementById("DualBag")
 let buyspicybag = 0
-let buyspicybagAmmount = document.getElementById("buyspicybag")
+let buyspicybagAmmount = document.getElementById("SpicyBag")
 let cps = 0
 let cpsAmmount = document.getElementById("cps")
+let bagCost = document.getElementById("bagcost")
+let bigbagCost = document.getElementById("bigbagcost")
+let dualbagCost = document.getElementById("dualbagcost")
+let spicybagCost = document.getElementById("spicybagcost")
 
 function increasePoints() {
     points = points + 1 + (buybag * .2)
     pointsTracker.innerText = points.toFixed(2) + " Skittles"
 }
 function purchasebuybag() {
+    let cost = 10 + buybag ** 1.5
     if (points >= 10) {
         points = points - 10
         pointsTracker.innerText = points.toFixed(2) + " Skittles"
         buybag = buybag + 1
         buybagAmmount.innerText = buybag + " Bags Owned"
+        bagCost.innerText = cost = 10 + buybag ** 1.5
     } else {
         alert("Not enough skittles! You need at least 10 skittles to buy a Bag")
     }
@@ -31,6 +37,7 @@ function purchasebuybigbag() {
         pointsTracker.innerText = points.toFixed(2) + " Skittles"
         buybigbag = buybigbag + 1
         buybigbagAmmount.innerText = buybigbag + " Big Bags Owned"
+        bigbagCost.innerText = cost = 10 + bigbag ** 1.5
     } else {
         alert("Not enough skittles! You need at least 50 skittles to buy Big Bags")
     }
@@ -41,6 +48,7 @@ function purchasebuydualbag() {
         pointsTracker.innerText = points.toFixed(2) + " Dual Bag"
         buydualbag = buydualbag + 1
         buydualbagAmmount.innerText = buydualbag + " Dual Bags Owned"
+        dualbagCost.innerText = cost = 10 + dualbag ** 1.5
     } else {
         alert("Not enough skittles! You need at least 250 skittles to buy Dual Bags")
     }
@@ -51,14 +59,18 @@ function purchasebuyspicybag() {
         pointsTracker.innerText = points.toFixed(2) + " Skittles"
         buyspicybag = buyspicybag + 1
         buyspicybagAmmount.innerText = buyspicybag + " Spicy Bag Owned"
+        spicybagCost.innerText = cost = 10 + spicybag ** 1.5
     } else {
         alert("Not enough skittles! You need at least 500 skittles to buy Spicy Bags")
     }
 }
 
+
+
 function gameLoop() {
-    points = points + (1 * purchasebuybag) + (5 * purchasebuybigbag) + (10 * purchasebuydualbag)
-    cps = (1 * purchasebuybag) + (5 * purchasebuydualbag) + (10 * purchasebuyspicybag)
+let buybag = 0
+    points = points + (1 * buybag) + (5 * buybigbag) + (10 * buydualbag)
+    cps = (1 * buybag) + (5 * buydualbag) + (10 * buyspicybag)
     pointsTracker.innerText = points.toFixed(2) + " Skittles"
     cpsAmmount.innerText = cps + " Skittles per second"
 }
